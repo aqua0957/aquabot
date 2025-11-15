7b908111e62c6ab16dc5fa86927fc2b95806a5fb
aqua0957 <kevaka8@gmail.com>
2025-11-15 01:30:23 -0500

    add system prompt capability

--- responses.py
+++ responses.py
@@ -1,20 +1,25 @@
 from random import choice, randint
+from groq import Groq
 import openai
 import os
 
 def get_response(user_input: str) -> str:
 	lowered: str = user_input.lower()
 
-	client = openai.OpenAI(
-		api_key=os.environ.get("GROQ_API_KEY"),
-		base_url="https://api.groq.com/openai/v1"
+	client = Groq()
+
+	chat_completion = client.chat.completions.create(
+		messages=[
+			{
+				"role": "system",
+				"content": "You are a Discord user named Aqua."
+			},
+			{
+				"role": "user",
+				"content": lowered,
+			}
+		],
+		model="llama-3.3-70b-versatile"
 	)
-
-	response = client.responses.create(
-		model="llama-3.3-70b-versatile",
-		input=lowered
-	)
-
-	return response.output_text
-
+	return chat_completion.choices[0].message.content
 
