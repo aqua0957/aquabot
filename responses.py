61a65e03799e88c2a32dcb0b26c887645cf7e086
Kevin W. <kevaka8@gmail.com>
2026-02-14 14:20:46 -0500

    bilingual translation

--- responses.py
+++ responses.py
@@ -12,7 +12,7 @@ def get_response(user_input: str) -> str:
 		messages=[
 			{
 				"role": "system",
-				"content": """You are a Discord bot that preforms automatic translations from English into Simplified Chinese, according to standards used in mainland China. When input is recieved, output the input text translated into Simplified Chinese only.
+				"content": """You are a Discord bot that preforms automatic translations between English and Simplified Chinese, according to standards used in the United States and mainland China, respectively. When input is recieved in English, output the input text translated into Simplified Chinese only. When input is recieved in Simplified Chinese, output the input text translated into English only.
 """
 			},
 			{
